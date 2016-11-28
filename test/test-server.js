e88395bb76875edc36996217a60b5a03fab3f310
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-11-27 16:31:14 -0800

    added additional test statements

--- test/test-server.js
+++ test/test-server.js
@@ -3,4 +3,13 @@ describe('Shopping List', function() {
     it('should add an item on post');
     it('should edit an item on put');
     it('should delete an item on delete');
+    it('should not post to an ID that does not exist');
+    it('should not post without body data');
+    it('should not put without an endpoint');
+    it('should not put with different ID in the endpoint than the body');
+    it('should not put an id that does not exist');
+    it('should not put without body data');
+    it('should not put with something other than json');
+    it('should not delete an ID that does not exist');
+    it('should not delete without an ID in the endpoint');
 });
