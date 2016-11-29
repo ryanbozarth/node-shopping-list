74e52ee040787b5223304fbab1e3904c1dbcf124
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-11-28 21:35:43 -0800

    added more unit tests

--- test/test-server.js
+++ test/test-server.js
@@ -31,50 +31,112 @@ describe('Shopping List', function() {
     });
     it('should add an item on post', function(done) {
         chai.request(app)
-        .get('/items')
-        .end(function(err, res) {
-            should.equal(err, null);
-            res.should.have.status(201);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property('name');
-            res.body.should.have.property('id');
-            res.body.name.should.be.a('string');
-            res.body.id.should.be.a('number');
-            res.body.name.should.equal('Kale');
-            storage.items.should.be.a('array');
-            storage.items.should.have.length(4);
-            storage.items[3].should.be.a('object');
-            storage.items[3].should.have.property('id');
-            storage.items[3].should.have.property('name');
-            storage.items[3].id.should.be.a('number');
-            storage.items[3].name.should.be.a('string');
-            storage.items[3].name.should.equal('Kale');
-            done();
-        });
+            .post('/items')
+            .send({'name': 'Kale'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('Kale');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('Kale');
+                done();
+            });
     });
-    it('should edit an item on put', function(done){
+    it('should edit an item on put', function(done) {
         chai.request(app)
-        .get('/items/0')
-        .end(function(err, res){
-        should.equal(err, null);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.name.should.be.equal('Broad beans');
-        res.body.should.have.property('id');
-        
-        
-        done();
-        });
+            .put('/items/0')
+            .send({'name': 'Green beans'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.name.should.be.equal('Green beans');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                done();
+            });
     });
-    it('should delete an item on delete');
+    it('should delete an item on delete', function(done) {
+        chai.request(app)
+            .delete('/items/0')
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                done();
+            });
+    });
     it('should not post to an ID that does not exist');
-    it('should not post without body data');
+    it('should not post without body data', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({})
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.body.should.not.have.property('name');
+                done();
+            });
+    });
     it('should not put without an endpoint');
-    it('should not put with different ID in the endpoint than the body');
-    it('should not put an id that does not exist');
-    it('should not put without body data');
-    it('should not put with something other than json');
-    it('should not delete an ID that does not exist');
-    it('should not delete without an ID in the endpoint');
+    it('should not put with different ID in the endpoint than the body', function(done) {
+        chai.request(app)
+            .put('/items/0')
+            .send({'name': 'coffee', 'id': 4})
+            .end(function(err, res) {
+                res.should.have.status(400);
+                done();
+            });
+    });
+    it('should not put an id that does not exist', function(done) {
+        chai.request(app)
+            .put('/items')
+            .send({'name': 'coffee', 'id': 4})
+            .end(function(err, res){
+                res.should.have.status(400);
+                done();
+            });
+    });
+    it('should not put without body data', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({})
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.body.should.not.have.property('name');
+            });
+    });
+    it('should not put with something other than json', function(done) {
+        chai.request(app)
+            .put('/items')
+            .end(function(err, res) {
+                res.should.be.json;
+            });
+    });
+    it('should not delete an ID that does not exist', function(done) {
+        chai.request(app)
+            .delete('/items/6')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                done();
+            });
+    });
+    it('should not delete without an ID in the endpoint', function(done) {
+        chai.request(app)
+            .delete('/items/')
+            .end(function(err, res) {
+                res.should.have.status(404);
+                done();
+            });
+    });
 });
